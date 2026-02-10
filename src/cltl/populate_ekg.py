34c08153f5b790b994733f30394712f202df60ae
piekvossen <piekvossen@gmail.com>
2026-02-10 20:18:11 +0100

    First commit

--- src/cltl/populate_ekg.py
+++ src/cltl/populate_ekg.py
@@ -10,12 +10,7 @@ import events_to_capsules
 from dateutil import parser
 
 
-def main():
-    f = open("/Users/piek/Desktop/Diabetes/load_datasets/diabetes/event_srl.json", "r")
-    annotated_conversations = json.load(f)
-    print(len(annotated_conversations))
-    print(annotated_conversations[0])
-
+def get_scenarios_from_srl_annotations(annotated_conversations):
     # Define contextual features
     place_id = getrandbits(8)
     location = requests.get("https://ipinfo.io").json()
@@ -37,9 +32,15 @@ def main():
             if capsules:
                 scenario = (scenario_context, capsules)
                 scenarios.append(scenario)
+    return scenarios
 
-    # for (context_capsule, content_capsules) in tqdm(scenarios):
-    #     print(context_capsule['context_id'], len(content_capsules))
+def main():
+    f = open("/Users/piek/Desktop/Diabetes/load_datasets/diabetes/event_srl.json", "r")
+    annotated_conversations = json.load(f)
+    print(len(annotated_conversations))
+    print(annotated_conversations[0])
+    scenarios = get_scenarios_from_srl_annotations(annotated_conversations)
+    print(len(scenarios))
 
     # Create folders
     scenario_filepath = Path('/Users/piek/Desktop/Diabetes/load_datasets//diabetes/')
@@ -53,34 +54,19 @@ def main():
 
     # Loop through capsules
     all_capsules = []
-    all_responses = []
     for (context_capsule, content_capsules) in tqdm(scenarios):
-        #print(f"\n\n---------------------------------------------------------------\n")
-
         print(context_capsule['context_id'], len(content_capsules))        # Create context
         response = brain.capsule_context(context_capsule)
 
         # Add information to the brain
         for capsule in content_capsules:
-            print('content_capsule', capsule["author"])
+            all_capsules.append(capsule)
             if capsule['utterance_type'] == UtteranceType.STATEMENT:
-              #  print(capsule)
                 response = brain.capsule_statement(capsule, reason_types=True, return_thoughts=False, create_label=True)
-              #  print(f"\nTriple: {capsule['triple']}\n")
-            # if capsule['utterance_type'] == UtteranceType.EXPERIENCE:
-            #     response = brain.capsule_experience(capsule, create_label=True)
-            #     print(f"\nEntity: {capsule['entity']}\n")
 
-            # capsule_json = brain_response_to_json(capsule)
-            # all_capsules.append(capsule_json)
-            # response_json = brain_response_to_json(response)
-            # all_responses.append(response_json)
-
-    # Save responses
+    # Save capsules
     f = open(scenario_filepath / "capsules.json", "w")
     json.dump(all_capsules, f)
-    # f = open(scenario_filepath / "responses.json", "w")
-    # json.dump(all_responses, f)
 
 if __name__ == '__main__':
-    pass
+    main()
